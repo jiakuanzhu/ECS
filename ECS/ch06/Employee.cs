6c94d39f4303916f72b64742888692794dbb11f8
Jiakuan ZHU <jiakuan.zhu1205@gmail.com>
2019-05-25 17:02:18 +0800

    Add class Employee

---
+++ ECS/ch06/Employee.cs
@@ -0,0 +1,9 @@
+namespace ch06
+{
+    public class Employee
+    {
+         public string FirstName;
+         public string LastName;
+         public string Salary = "Not enough";
+    }
+}
--- ECS/ch06/Program.cs
+++ ECS/ch06/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+//using ch06.Employee;
 
 namespace ch06
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            
+            Employee employee1 = new Employee();
+            Employee employee2;
+            employee2 = new Employee();
+
+            employee1.FirstName = "Inigo";
+            employee1.LastName = "Montoya";
+            employee1.Salary = "Too Little";
+            IncreaseSalary(employee1);
+            Console.WriteLine(
+                "{0} {1}: {2}",
+                employee1.FirstName,
+                employee1.LastName,
+                employee1.Salary);
+        }
+        
+        static void IncreaseSalary(Employee employee)
+        {
+            employee.Salary = "Enough to survive on";
         }
     }
 }
